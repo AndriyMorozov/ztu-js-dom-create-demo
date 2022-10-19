7f8343ba8a5d4983b3ed80c6c7d080edcf65afc8
Andriy Morozov <morozov@ztu.edu.ua>
2022-10-19 17:43:18 +0300

    demo

--- main.js
+++ main.js
@@ -1,33 +1,94 @@
 
 
-function stringInversion(str) {
-    let wordsArray = str.split(' ');
-    wordsArray.reverse();
-    return wordsArray.join(' ');
-}
-function searchDatesInString(str) {
-    let dateRegExp = /(\d{2})-(\d{2})-(\d{4})/;
-    let res = str.match(dateRegExp);
-    return `день: ${res[1]}, місяць: ${res[2]}, рік: ${res[3]}`;
+
+
+/*
+function createPanel(title, content) {
+    let resultString = '<div class="panel">';
+    resultString += '<div class="head">';
+    resultString += `<div class="title">${title}</div>`;
+    resultString += `<div class="buttons">`;
+    resultString += `<div class="button min"></div>`;
+    resultString += `</div>`;
+    resultString += '</div>';
+    resultString += '<div class="body">';
+    resultString += content;
+    resultString += '</div>';
+    resultString += '</div>';
+    return resultString;
 }
 
-let but = document.querySelector('#but');
-but.addEventListener('click',
-    (event) => {
-        // var 1
-        let textElement = document.forms['demoform'].text;
-        // var 2
-        let textElement2 = document.querySelector('#text');
-        // var 3
-        let textElement3 = document.querySelector("form[name='demoform'] input[name='text']");
-        // var 4
-        let textElement4 = document.forms.demoform.text;
-        let str = textElement.value;
-        let invertedString = stringInversion(str);
-        let resultElement = document.querySelector("form[name='demoform'] .result");
-        resultElement.innerHTML = invertedString;
-        let dateString = searchDatesInString(str);
-        document.querySelector('#dates-result').innerHTML = dateString;
+document.body.innerHTML += createPanel('New Panel', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid, architecto commodi dignissimos et eum illo impedit incidunt ipsam iusto minus nisi veniam voluptatibus? Corporis deleniti molestiae officiis repellat ullam.');
+document.body.innerHTML += createPanel('New Panel', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid, architecto commodi dignissimos et eum illo impedit incidunt ipsam iusto minus nisi veniam voluptatibus? Corporis deleniti molestiae officiis repellat ullam.');
+document.body.innerHTML += createPanel('New Panel', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid, architecto commodi dignissimos et eum illo impedit incidunt ipsam iusto minus nisi veniam voluptatibus? Corporis deleniti molestiae officiis repellat ullam.');
+*/
+let panelCount = 0;
+function createPanel(title, content, element = document.body) {
+    let panelNumber = panelCount;
+    let divPanel = document.createElement('div');
+    divPanel.classList.add('panel');
+    divPanel.dataset.number = panelNumber;
+    let divHead = document.createElement('div');
+    divHead.classList.add('head');
+    let divTitle = document.createElement('div');
+    divTitle.classList.add('title');
+    divTitle.innerHTML = title;
+    let divButtons = document.createElement('div');
+    divButtons.classList.add('buttons');
+    let divMinButton = document.createElement('div');
+    divMinButton.classList.add('button');
+    divMinButton.classList.add('min');
+    let divBody = document.createElement('div');
+    divBody.classList.add('body');
+    divBody.innerHTML = content;
 
+    divButtons.appendChild(divMinButton);
+
+    divHead.appendChild(divTitle);
+    divHead.appendChild(divButtons);
+    divPanel.appendChild(divHead);
+    divPanel.appendChild(divBody);
+
+    divMinButton.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.classList.contains('button')) {
+            let elem = target.parentNode.parentNode.nextElementSibling;
+            elem.classList.toggle('hidden');
+            if (elem.classList.contains('hidden'))
+                localStorage.setItem(`panel-${panelNumber}`, 'hidden');
+            else
+                localStorage.setItem(`panel-${panelNumber}`, 'visible');
+        }
     });
 
+    element.appendChild(divPanel);
+    panelCount++;
+    return divBody;
+}
+function showPanel(number) {
+    let panel = document.querySelector(`.panel[data-number='${number}']`);
+    let body = panel.querySelector('.body');
+    body.classList.remove('hidden');
+    localStorage.setItem(`panel-${number}`, 'visible');
+}
+function hidePanel(number) {
+    let panel = document.querySelector(`.panel[data-number='${number}']`);
+    let body = panel.querySelector('.body');
+    body.classList.add('hidden');
+    localStorage.setItem(`panel-${number}`, 'hidden');
+}
+function loadPanels() {
+    for(let i = 0; i < panelCount; i++) {
+        let state = localStorage.getItem(`panel-${i}`);
+        if (state === 'hidden')
+            hidePanel(i);
+    }
+}
+let panelBodyOne = createPanel('Demo', 'One');
+let panelBodyTwo = createPanel('Demo', 'Two', panelBodyOne);
+createPanel('Demo', '3', panelBodyTwo);
+createPanel('Demo', 'One');
+createPanel('Demo', 'One');
+createPanel('Demo', 'One');
+
+loadPanels();
